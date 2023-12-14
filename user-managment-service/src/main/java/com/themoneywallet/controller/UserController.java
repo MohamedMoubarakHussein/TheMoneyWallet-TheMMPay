93ad98897f1c0fcd3c6a73fd10c69a3a8510ce34
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2023-12-14 10:48:50 +0200

    Updating userController and adding application properties

--- user-managment-service/src/main/java/com/themoneywallet/controller/UserController.java
+++ user-managment-service/src/main/java/com/themoneywallet/controller/UserController.java
@@ -2,17 +2,35 @@ package com.themoneywallet.controller;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.themoneywallet.dto.request.UserRequest;
+
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @RestController
 @RequestMapping("/user")
-@Slf4j
 @RequiredArgsConstructor
 public class UserController {
     
+    @PostMapping("/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public String signUp(@RequestBody UserRequest user){
+        return "";
+    }
+
+
+    @GetMapping("/getbyemail")
+    public String getUserByEmail(@RequestParam("email") String email){
+        return "";
+    }
     
 }
