d818e164fbaae21937554ef9f80ef78e6e832a89
Ahmed Mubarak <ahmedmubarak.hsin@gmail.com>
2023-12-14 11:43:33 +0200

    Updating the structure of the controller

--- user-managment-service/src/main/java/com/themoneywallet/usermanagmentservice/controller/UserController.java
+++ user-managment-service/src/main/java/com/themoneywallet/usermanagmentservice/controller/UserController.java
@@ -5,23 +5,19 @@ import org.springframework.web.bind.annotation.RestController;
 import com.themoneywallet.usermanagmentservice.dto.request.UserRequest;
 
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 
 @RestController
 @RequestMapping("/user")
 @RequiredArgsConstructor
-@CrossOrigin(origins =   "*")
 public class UserController {
     
     @PostMapping("/create")
@@ -34,14 +30,25 @@ public class UserController {
     @GetMapping("/getbyemail")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public String getUserByEmail(@RequestParam("email") String email){
-        return "zz";
+       return "";
     }
 
-      @GetMapping("/z")
+  @GetMapping("/getbyusername")
     @ResponseStatus(HttpStatus.ACCEPTED)
-    @ResponseBody
-    public String getUserByEmailz(){
-        return "zz";
+    public String getUserByUserName(@RequestParam("usr") String email){
+       return "";
+    }
+
+     @GetMapping("/getbyemail")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String updateUser(@RequestParam("email") String email){
+       return "";
+    }
+
+ @GetMapping("/getbyemail")
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public String deleteUser(@RequestParam("email") String email){
+       return "";
     }
     
 }
--- user-managment-service/src/main/java/com/themoneywallet/usermanagmentservice/repository/UserRepository.java
+++ user-managment-service/src/main/java/com/themoneywallet/usermanagmentservice/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.themoneywallet.usermanagmentservice.repository;
 
 import java.util.Optional;
-import java.util.UUID;
+
 
 import org.springframework.data.repository.CrudRepository;
 
@@ -9,6 +9,5 @@ import com.themoneywallet.usermanagmentservice.entity.User;
 
 public interface UserRepository extends CrudRepository<User , Integer> {
     Optional<User> findByUserName(String username);
-    Optional<User> findByUser_id(UUID userId);
     Optional<User> findByEmail(String email);
 }
