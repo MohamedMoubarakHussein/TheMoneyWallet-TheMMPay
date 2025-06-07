2aaefa6ff6337dcb98d6c662330bbdc73d1750d2
Mohamed moubarak <mohmedmoberk64@gmail.com>
2025-06-08 00:48:36 +0300

    Updating the opened paths to includes the swager for dev

--- Back-end/gateway/src/main/java/com/gatewayApi/Service/OpenedAndSecuredPathsService.java
+++ Back-end/gateway/src/main/java/com/gatewayApi/Service/OpenedAndSecuredPathsService.java
@@ -1,22 +1,28 @@
 package com.gatewayApi.Service;
 
 import java.util.List;
-
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Service;
 
 @Service
 public class OpenedAndSecuredPathsService {
+
     private static final List<String> openEndPoints = List.of(
-        "/auth/signup" ,
+        "/auth/signup",
         "/auth/signin",
         "/auth/logout",
-        "/auth/refreshtoken"
+        "/auth/refreshtoken",
+        "/auth/v3/api-docs",
+        "/auth/v3/api-docs/**",
+        "/auth/swagger-ui.html",
+        "/auth/swagger-ui/index.html",
+        "/auth/swagger-ui/**",
+        "/auth/webjars/**"
     );
 
-    public Boolean isSecuredEndPoint(ServerHttpRequest request){
-        return OpenedAndSecuredPathsService.openEndPoints.stream()
-                    .noneMatch(url -> request.getURI().getPath().contains(url));
+    public Boolean isSecuredEndPoint(ServerHttpRequest request) {
+        return OpenedAndSecuredPathsService.openEndPoints
+            .stream()
+            .noneMatch(url -> request.getURI().getPath().contains(url));
     }
-
 }
