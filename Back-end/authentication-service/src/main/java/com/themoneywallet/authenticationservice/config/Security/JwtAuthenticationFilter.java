2aa20e2c3ac97628de147a207f0c5324f1c307e9
Mohamed moubarak <mohmedmoberk64@gmail.com>
2025-07-22 09:13:04 +0300

    Removing auto generating  access token from  refresh token and delgate it to front to asks for generating

--- Back-end/authentication-service/src/main/java/com/themoneywallet/authenticationservice/config/Security/JwtAuthenticationFilter.java
+++ Back-end/authentication-service/src/main/java/com/themoneywallet/authenticationservice/config/Security/JwtAuthenticationFilter.java
@@ -1,6 +1,5 @@
 package com.themoneywallet.authenticationservice.config.Security;
 
-import com.themoneywallet.authenticationservice.service.implementation.JwtRefService;
 import com.themoneywallet.authenticationservice.service.implementation.JwtService;
 import com.themoneywallet.authenticationservice.service.implementation.MyUserDetailsService;
 import jakarta.servlet.FilterChain;
@@ -8,7 +7,6 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -24,7 +22,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
     private final JwtService jwtService;
-    private final JwtRefService jwtRefService;
     private final MyUserDetailsService userDetailsService;
 
     @Override
@@ -36,6 +33,7 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         String header = request.getHeader("Authorization");
 
         if (
+            header == null ||
             StringUtils.isEmpty(header) ||
             !StringUtils.startsWith(header, "Bearer ")
         ) {
@@ -71,49 +69,8 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                 }
             }
         } catch (Exception e) {
-            Optional<String> refreshTokenOpt =
-                this.jwtRefService.extractRefreshTokenFromCookies(request);
+              logger.error("JWT authentication error: " + request.getRequestURL().toString());
 
-            if (refreshTokenOpt.isPresent()) {
-                String refreshToken = refreshTokenOpt.get();
-                if (this.jwtRefService.isTokenValid(refreshToken)) {
-                    String refreshUsername =
-                        this.jwtRefService.extractUserName(refreshToken);
-
-                    String newAccessToken =
-                        this.jwtService.generateToken(refreshUsername , null);
-
-                    // Add the new token to the response
-                    this.jwtRefService.addNewAccessTokenCookie(
-                            response,
-                            newAccessToken
-                        );
-                    SecurityContext context =
-                        SecurityContextHolder.createEmptyContext();
-                    UserDetails user =
-                        this.userDetailsService.loadUserByUsername(
-                                refreshUsername
-                            );
-
-                    UsernamePasswordAuthenticationToken auth =
-                        new UsernamePasswordAuthenticationToken(
-                            user,
-                            null,
-                            user.getAuthorities()
-                        );
-                    auth.setDetails(
-                        new WebAuthenticationDetailsSource()
-                            .buildDetails(request)
-                    );
-                    context.setAuthentication(auth);
-                    SecurityContextHolder.setContext(context);
-
-                    response.setHeader(
-                        "Authorization",
-                        "Bearer " + newAccessToken
-                    );
-                }
-            }
         }
 
         filterChain.doFilter(request, response);
